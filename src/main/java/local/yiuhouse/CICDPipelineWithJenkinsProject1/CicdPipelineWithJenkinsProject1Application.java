f443a327d404a6fa06d103709e3855eb9b35c45a
William Yiu <wyiu@yiuhouse.local>
2022-02-11 23:33:15 -0500

    Initial commit

---
+++ src/main/java/local/yiuhouse/CICDPipelineWithJenkinsProject1/CicdPipelineWithJenkinsProject1Application.java
@@ -0,0 +1,13 @@
+package local.yiuhouse.CICDPipelineWithJenkinsProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPipelineWithJenkinsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPipelineWithJenkinsProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/local/yiuhouse/CICDPipelineWithJenkinsProject1/CicdPipelineWithJenkinsProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package local.yiuhouse.CICDPipelineWithJenkinsProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineWithJenkinsProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
